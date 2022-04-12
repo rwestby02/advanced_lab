d195dba5cbb07f8b508559672250375fe6c55073
rwestby02 <102184328+rwestby02@users.noreply.github.com>
2022-04-12 14:38:31 -0500

    images display correctly, added basic histogram, want 3D histogram

--- lead_detection.py
+++ lead_detection.py
@@ -10,29 +10,31 @@ from skimage.util import img_as_ubyte
 
 #print(image_loc)
 
-image_read = io.imread(r"C:\Users\riley\Downloads\1pixelyellow.jpg")#the letter r ensures that the filepath can be read on windows, using skimage b/c problems with opencv
+image_read = io.imread(r"C:\Users\riley\Pictures\townscaper.jpeg")#the letter r ensures that the filepath can be read on windows, using skimage b/c problems with opencv
 image_input = img_as_ubyte(image_read)
-image_hsv = cv.cvtColor(image_input, cv.COLOR_RGB2HSV)#color conversion
+image_rgb = cv.cvtColor(image_input, cv.COLOR_BGR2RGB)
+image_hsv = cv.cvtColor(image_input, cv.COLOR_BGR2HSV)#color conversion
 
-lower_bound = np.array([25, 10, 10])#selecting for yellow values, needs refinement. HSV in opencv is 0-179,0-255,0-255   
-upper_bound = np.array([29, 255, 255])
+lower_bound = np.array([88, 10, 10])#selecting for yellow values, needs refinement. HSV in opencv is 0-179,0-255,0-255   
+upper_bound = np.array([96, 255, 255])
 
 mask = cv.inRange(image_hsv, lower_bound, upper_bound)#binary image of pixels with yellow values, black is no yellow, white is yellow
 
 result = cv.bitwise_and(image_hsv, image_hsv, mask = mask)#combines the image with the mask to cut out all the errors from hsv conversion
 
-cv.namedWindow("Display window", cv.WINDOW_NORMAL)#displays all the created images
+cv.namedWindow("HSV", cv.WINDOW_NORMAL)#displays all the created images
 cv.namedWindow('Mask', cv.WINDOW_NORMAL)
 cv.namedWindow('Result', cv.WINDOW_NORMAL)
-cv.namedWindow('input', cv.WINDOW_NORMAL)
-cv.imshow('input', image_input)
-cv.imshow("Display window", image_hsv)
+cv.namedWindow('Input', cv.WINDOW_NORMAL)
+cv.imshow('Input', image_rgb)
+cv.imshow("HSV", image_hsv)
 cv.imshow('Mask', mask)
 cv.imshow('Result', result)
 
-print(image_input)
-print(image_hsv)
-
+#print(image_input)
+#print(image_hsv)
+plt.hist(result.ravel(),256,[1,254]); plt.show()
 
 cv.waitKey(0)
 cv.destroyAllWindows()#pressing any key closes all the windows, waits indefinitely 
+
