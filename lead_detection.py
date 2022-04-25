abfefff6c997d18d5f475f11ef17e1abbce1a8fd
rwestby02 <102184328+rwestby02@users.noreply.github.com>
2022-04-25 12:53:46 -0500

    Update lead_detection.py

--- lead_detection.py
+++ lead_detection.py
@@ -1,40 +1,46 @@
-import numpy as np
-import cv2 as cv
-from matplotlib import pyplot as plt
-from skimage import io
-from skimage.util import img_as_ubyte
+import numpy as np #images are stored as arrays
+import cv2 as cv #image processing
+from matplotlib import pyplot as plt #histograms
+from skimage import io #for importing images
+from skimage.util import img_as_ubyte #for importing images and proper interfacing with opencv
 
-
-#print("image filepath")  #trying to ensure that the r to make raw strings is added to an inputted string, windows problem
-#image_loc = input()
-
-#print(image_loc)
-
-image_read = io.imread(r"C:\Users\riley\Pictures\townscaper.jpeg")#the letter r ensures that the filepath can be read on windows, using skimage b/c problems with opencv
+image_read = io.imread(r"filepath")#the letter r ensures that the filepath can be read on windows, using skimage b/c problems with opencv
 image_input = img_as_ubyte(image_read)
 image_rgb = cv.cvtColor(image_input, cv.COLOR_BGR2RGB)
-image_hsv = cv.cvtColor(image_input, cv.COLOR_BGR2HSV)#color conversion
+image_hsv = cv.cvtColor(image_input, cv.COLOR_BGR2HSV) #color conversion
 
-lower_bound = np.array([88, 10, 10])#selecting for yellow values, needs refinement. HSV in opencv is 0-179,0-255,0-255   
-upper_bound = np.array([96, 255, 255])
 
-mask = cv.inRange(image_hsv, lower_bound, upper_bound)#binary image of pixels with yellow values, black is no yellow, white is yellow
+lower_bound = np.array([88, 50, 20]) #selecting for yellow values, needs refinement. HSV in opencv is 0-179,0-255,0-255. yellow about 85 - 95   
+upper_bound = np.array([97, 254, 254])
 
-result = cv.bitwise_and(image_hsv, image_hsv, mask = mask)#combines the image with the mask to cut out all the errors from hsv conversion
+mask = cv.inRange(image_hsv, lower_bound, upper_bound) #binary image of pixels with yellow values, black is no yellow, white is yellow
 
-cv.namedWindow("HSV", cv.WINDOW_NORMAL)#displays all the created images
-cv.namedWindow('Mask', cv.WINDOW_NORMAL)
-cv.namedWindow('Result', cv.WINDOW_NORMAL)
-cv.namedWindow('Input', cv.WINDOW_NORMAL)
-cv.imshow('Input', image_rgb)
-cv.imshow("HSV", image_hsv)
-cv.imshow('Mask', mask)
-cv.imshow('Result', result)
+result = cv.bitwise_and(image_rgb, image_rgb, mask = mask) #combines the image with the mask to cut out all the errors from hsv conversion
 
-#print(image_input)
-#print(image_hsv)
-plt.hist(result.ravel(),256,[1,254]); plt.show()
+pregray = cv.cvtColor(result, cv.COLOR_HSV2RGB) #hsv to gray direct conversion does not exist
+gray = cv.cvtColor(pregray, cv.COLOR_RGB2GRAY) #makes grayscale image of yellow pixels
 
-cv.waitKey(0)
-cv.destroyAllWindows()#pressing any key closes all the windows, waits indefinitely 
+yellow_intensity = np.sum(gray) #yellow intensity as measured by amount of yellow, not scaled for image size
 
+
+cv.imwrite("mask.jpg", mask) #saves result images for debugging
+cv.imwrite("result.jpg", result)
+cv.imwrite("gray.jpg", gray)
+
+  #displays all the created images, for debugging
+#cv.namedWindow('Mask', cv.WINDOW_NORMAL)
+#cv.namedWindow('Result', cv.WINDOW_NORMAL)
+#cv.namedWindow('Input', cv.WINDOW_NORMAL)
+#cv.namedWindow('Gray', cv.WINDOW_NORMAL)
+#cv.imshow('Input', image_rgb)
+#cv.imshow('Mask', mask)
+#cv.imshow('Result', result)
+#cv.imshow('Gray', gray)
+
+
+print(yellow_intensity) #prints the yellow intensity value
+#plt.hist(result.ravel(),256,[1,254]); plt.show() #2d yellow historgram
+
+
+#cv.waitKey(0)
+#cv.destroyAllWindows() #pressing any key closes all the windows, waits indefinitely 
